38857477301b35c1e40b61910d05900f27acbf91
IAMPATTRIXX <dexza53@gmail.com>
2020-04-18 02:33:11 +0700

    better than

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -93,7 +93,7 @@ export default class Profile extends React.Component {
             }
 
             if(String(this.state.status).localeCompare("navbar")!=0){
-                this.props.addBooing(room);
+                this.props.addBooking(room);
             }
             const exist = localStorage.getItem('token')
             if(exist!=null){
@@ -211,80 +211,3 @@ export default class Profile extends React.Component {
         )
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-//     function Signin (){
-//     return(
-//         <div id="col-100">
-//             <NavbarSign />
-//             <div className = "center"> {/* ---------------------------------------------------------------- Sign in page  */}
-//                 <div className = "nameHotel" id="titleSign">
-//                     . s i g n &nbsp; i n
-//                 </div> 
-//                      <form action="" className="form" > 
-//                         <div className="col-25">  
-//                             <label className="forms">Name: </label>
-//                         </div>
-//                         <div className="col-75">
-//                             <input type="text" name="fname" placeholder="Jacob"/>
-//                         </div>
-//                         <div className="col-25">
-//                             <label className="forms">Surname: </label>
-//                         </div>
-//                         <div className="col-75">
-//                             <input type="text" name="lname" placeholder="Wilton"/>
-//                         </div>
-//                         <div className="col-25">
-//                             <label className="forms"> Email: </label>
-//                         </div>
-//                         <div className="col-75">
-//                             <input type="email" placeholder="Jacob@example.com"/>
-//                         </div>
-//                         <div className="col-25">
-//                             <label className="forms"> Password: </label>
-//                         </div>
-//                         <div className="col-75">
-//                             <input type="password" />
-//                         </div>  
-//                         <div className="col-25">
-//                             <label className="forms"> Confirm password:</label>
-//                         </div>
-//                         <div className="col-75">
-//                             <input type="password" />
-//                         </div>
-//                         <div className="confirmSigninLabel">
-//                             <label for="confirmSignin" > My information is correct.</label>     
-//                         </div>
-//                         <div className="checkbox" >
-//                             <input type="checkbox" className="confirmSignin" name="confirmSignin"/> 
-//                         </div>
-//                         <div className="col-100">
-//                         <Link to="/Home"><input type="submit" id="submit" value="SIGN IN"/> {/* login */}
-//                         </Link>
-//                         </div>
-//                      </form>  
-//                 </div>
-//                 <div className="col-100Foot" >
-//                     <div className="footer" id="homeFoot"> {/* ------------------------------------------------------------------------- footer send to about us page */}
-//                         . h o t e l  &nbsp;&nbsp; | &nbsp;&nbsp;
-//                         <button className="footerBtn">
-//                         a b o u t &nbsp;&nbsp;&nbsp;&nbsp;u s
-//                         </button>
-//                     </div>
-//                 </div>
-
-//         </div>       
-
-//         )
-//     }
-
-// export default Signin 
